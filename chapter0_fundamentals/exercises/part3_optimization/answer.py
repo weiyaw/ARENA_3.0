22739c37abb24f101c9761d676f13d7a9d47f1db
Kenyon Ng <kenyon.ng@gmail.com>
2026-03-28 20:32:28 +1100

    Update chap 1

---
+++ chapter0_fundamentals/exercises/part3_optimization/answer.py
@@ -0,0 +1,183 @@
+# %%
+import os
+import sys
+import time
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Callable, Iterable, Literal
+
+import numpy as np
+import torch as t
+import torch.distributed as dist
+import torch.multiprocessing as mp
+import torch.nn.functional as F
+import wandb
+from IPython.core.display import HTML
+from IPython.display import display
+from jaxtyping import Float, Int
+from torch import Tensor, optim
+from torch.utils.data import DataLoader, DistributedSampler
+from torchvision import datasets, transforms
+from tqdm import tqdm
+
+import utils
+
+# Make sure exercises are in the path
+chapter = "chapter0_fundamentals"
+section = "part3_optimization"
+root_dir = next(p for p in Path.cwd().parents if (p / chapter).exists())
+exercises_dir = root_dir / chapter / "exercises"
+section_dir = exercises_dir / section
+if str(exercises_dir) not in sys.path:
+    sys.path.append(str(exercises_dir))
+
+MAIN = __name__ == "__main__"
+
+import part3_optimization.tests as tests
+from part2_cnns.solutions import Linear, ResNet34, get_resnet_for_feature_extraction
+from part3_optimization.utils import plot_fn, plot_fn_with_points
+from plotly_utils import bar, imshow, line
+
+device = t.device("mps" if t.backends.mps.is_available() else "cuda" if t.cuda.is_available() else "cpu")
+ #%%
+def pathological_curve_loss(x: Tensor, y: Tensor):
+    # Example of a pathological curvature. There are many more possible, feel free to experiment here!
+    x_loss = t.tanh(x) ** 2 + 0.01 * t.abs(x)
+    y_loss = t.sigmoid(y)
+    return x_loss + y_loss
+
+
+utils.plot_fn(pathological_curve_loss, min_points=[(0, "y_min")])
+
+# %%
+def opt_fn_with_sgd(
+    fn: Callable, xy: Float[Tensor, "2"], lr=0.001, momentum=0.98, n_iters: int = 100
+) -> Float[Tensor, "n_iters 2"]:
+    """
+    Optimize the a given function starting from the specified point.
+
+    xy: shape (2,). The (x, y) starting point.
+    n_iters: number of steps.
+    lr, momentum: parameters passed to the torch.optim.SGD optimizer.
+
+    Return: (n_iters+1, 2). The (x, y) values, from initial values to values after step `n_iters`.
+    """
+    # Make sure tensor has requires_grad=True, otherwise it can't be optimized (more on this tomorrow!)
+    assert xy.requires_grad
+    opt = t.optim.SGD([xy], lr=lr, momentum=momentum)
+
+    result = [xy.detach().clone()]
+    for i in range(n_iters):
+        opt.zero_grad()
+        loss = fn(xy[0], xy[1])
+        loss.backward()
+        opt.step()
+        result.append(xy.detach().clone())
+
+    return t.stack(result)
+
+
+points = []
+
+optimizer_list = [
+    (optim.SGD, {"lr": 0.1, "momentum": 0.0}),
+    (optim.SGD, {"lr": 0.02, "momentum": 0.99}),
+]
+
+for optimizer_class, params in optimizer_list:
+    xy = t.tensor([2.5, 2.5], requires_grad=True)
+    xys = opt_fn_with_sgd(pathological_curve_loss, xy=xy, lr=params["lr"], momentum=params["momentum"], n_iters=1000)
+    points.append((xys, optimizer_class, params))
+    print(f"{params=}, last point={xys[-1]}")
+
+plot_fn_with_points(pathological_curve_loss, points=points, min_points=[(0, "y_min")])
+
+# %%
+class SGD:
+    def __init__(
+        self,
+        params: Iterable[t.nn.parameter.Parameter],
+        lr: float,
+        momentum: float = 0.0,
+        weight_decay: float = 0.0,
+    ):
+        """Implements SGD with momentum.
+
+        Like the PyTorch version, but assume nesterov=False, maximize=False, and dampening=0
+            https://pytorch.org/docs/stable/generated/torch.optim.SGD.html#torch.optim.SGD
+        """
+        self.params = list(params)  # turn params into a list (it might be a generator, so iterating over it empties it)
+        self.lr = lr
+        self.mu = momentum
+        self.lmda = weight_decay
+
+        self.b = [t.zeros_like(p) for p in self.params]
+
+    def zero_grad(self) -> None:
+        """Zeros all gradients of the parameters in `self.params`."""
+        for param in self.params:
+            param.grad = None
+
+    @t.inference_mode()
+    def step(self) -> None:
+        """Performs a single optimization step of the SGD algorithm."""
+        grads = [param.grad for param in self.params]
+
+        for m, p in zip(self.b, self.params):
+            g = p.grad
+            if self.lmda != 0:
+                g += self.lmda * p
+            if self.mu != 0:
+                m.copy_(g + self.mu * m)
+                g = m
+            p -= self.lr * g
+
+
+    def __repr__(self) -> str:
+        return f"SGD(lr={self.lr}, momentum={self.mu}, weight_decay={self.lmda})"
+
+
+tests.test_sgd(SGD)
+# %%
+
+class RMSprop:
+    def __init__(
+        self,
+        params: Iterable[t.nn.parameter.Parameter],
+        lr: float = 0.01,
+        alpha: float = 0.99,
+        eps: float = 1e-08,
+        weight_decay: float = 0.0,
+        momentum: float = 0.0,
+    ):
+        """Implements RMSprop.
+
+        Like the PyTorch version, but assumes centered=False
+            https://pytorch.org/docs/stable/generated/torch.optim.RMSprop.html
+        """
+        self.params = list(params)  # turn params into a list (because it might be a generator)
+        self.lr = lr
+        self.eps = eps
+        self.mu = momentum
+        self.lmda = weight_decay
+        self.alpha = alpha
+
+        self.b = [t.zeros_like(p) for p in self.params]
+        self.v = [t.zeros_like(p) for p in self.params]
+
+    def zero_grad(self) -> None:
+        for p in self.params:
+            p.grad = None
+
+    @t.inference_mode()
+    def step(self) -> None:
+        raise NotImplementedError()
+
+    def __repr__(self) -> str:
+        return (
+            f"RMSprop(lr={self.lr}, eps={self.eps}, momentum={self.mu}, weight_decay={self.lmda}, alpha={self.alpha})"
+        )
+
+
+tests.test_rmsprop(RMSprop)
+
---
+++ chapter1_transformer_interp/exercises/part1_transformer_from_scratch/answers.py
@@ -0,0 +1,529 @@
+# %%
+import math
+import os
+import sys
+from collections import defaultdict
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Callable
+
+import datasets
+import einops
+import numpy as np
+import torch as t
+import torch.nn as nn
+# import wandb
+from jaxtyping import Float, Int
+from rich import print as rprint
+from rich.table import Table
+from torch import Tensor
+from torch.utils.data import DataLoader
+from tqdm.notebook import tqdm
+from transformer_lens import HookedTransformer
+from transformer_lens.utils import gelu_new, tokenize_and_concatenate
+from transformers import GPT2TokenizerFast
+
+device = t.device("mps" if t.backends.mps.is_available() else "cuda" if t.cuda.is_available() else "cpu")
+# device = "cpu"
+
+# Make sure exercises are in the path
+chapter = "chapter1_transformer_interp"
+section = "part1_transformer_from_scratch"
+root_dir = next(p for p in Path.cwd().parents if (p / chapter).exists())
+exercises_dir = root_dir / chapter / "exercises"
+section_dir = exercises_dir / section
+if str(exercises_dir) not in sys.path:
+    sys.path.append(str(exercises_dir))
+
+import part1_transformer_from_scratch.solutions as solutions
+import part1_transformer_from_scratch.tests as tests
+
+MAIN = __name__ == "__main__"
+
+
+
+# %%
+reference_gpt2 = HookedTransformer.from_pretrained(
+    "gpt2-small",
+    fold_ln=False,
+    center_unembed=False,
+    center_writing_weights=False,  # you'll learn about these arguments later!
+)
+
+sorted_vocab = sorted(list(reference_gpt2.tokenizer.vocab.items()), key=lambda n: n[1])
+
+print(sorted_vocab[:20])
+print()
+print(sorted_vocab[250:270])
+print()
+print(sorted_vocab[990:1010])
+print()
+
+# %%
+reference_text = "I am an amazing autoregressive, decoder-only, GPT-2 style transformer. One day I will exceed human level intelligence and take over the world!"
+tokens = reference_gpt2.to_tokens(reference_text).to(device)
+print(tokens)
+print(tokens.shape)
+print(reference_gpt2.to_str_tokens(tokens))
+# %%
+logits, cache = reference_gpt2.run_with_cache(tokens)
+print(logits.shape)
+# %%
+probs = logits.softmax(dim=-1)
+print(probs.shape)
+# %%
+
+for activation_name, activation in cache.items():
+    # Only print for first layer
+    if ".0." in activation_name or "blocks" not in activation_name:
+        print(f"{activation_name:30} {tuple(activation.shape)}")
+# %%
+
+for name, param in reference_gpt2.named_parameters():
+    # Only print for first layer
+    if ".0." in name or "blocks" not in name:
+        print(f"{name:18} {tuple(param.shape)}")
+# %%
+# As a reference - note there's a lot of stuff we don't care about in here, to do with library internals or other architectures
+print(reference_gpt2.cfg)
+# %%
+@dataclass
+class Config:
+    d_model: int = 768
+    debug: bool = True
+    layer_norm_eps: float = 1e-5
+    d_vocab: int = 50257
+    init_range: float = 0.02
+    n_ctx: int = 1024
+    d_head: int = 64
+    d_mlp: int = 3072
+    n_heads: int = 12
+    n_layers: int = 12
+
+
+cfg = Config()
+print(cfg)
+# %%
+def rand_float_test(cls, shape):
+    cfg = Config(debug=True)
+    layer = cls(cfg).to(device)
+    random_input = t.randn(shape).to(device)
+    print("Input shape:", random_input.shape)
+    output = layer(random_input)
+    if isinstance(output, tuple):
+        output = output[0]
+    print("Output shape:", output.shape, "\n")
+
+
+def rand_int_test(cls, shape):
+    cfg = Config(debug=True)
+    layer = cls(cfg).to(device)
+    random_input = t.randint(100, 1000, shape).to(device)
+    print("Input shape:", random_input.shape)
+    output = layer(random_input)
+    if isinstance(output, tuple):
+        output = output[0]
+    print("Output shape:", output.shape, "\n")
+
+
+def load_gpt2_test(cls, gpt2_layer, input):
+    cfg = Config(debug=True)
+    layer = cls(cfg).to(device)
+    layer.load_state_dict(gpt2_layer.state_dict(), strict=False)
+    print("Input shape:", input.shape)
+    orig_input = input.clone()
+    output = layer(orig_input)
+    assert t.allclose(input, orig_input), "Input has been modified, make sure operations are not done in place"
+    if isinstance(output, tuple):
+        output = output[0]
+    print("Output shape:", output.shape)
+    try:
+        reference_output = gpt2_layer(input)
+    except:
+        reference_output = gpt2_layer(input, input, input)
+    print("Reference output shape:", reference_output.shape, "\n")
+    comparison = t.isclose(output, reference_output, atol=1e-4, rtol=1e-3)
+    print(f"{comparison.sum() / comparison.numel():.2%} of the values are correct\n")
+    assert 1 - (comparison.sum() / comparison.numel()) < 1e-5, "More than 0.01% of the values are incorrect"
+
+# %%
+class LayerNorm(nn.Module):
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.w = nn.Parameter(t.ones(cfg.d_model))
+        self.b = nn.Parameter(t.zeros(cfg.d_model))
+
+    def forward(self, residual: Float[Tensor, "batch posn d_model"]) -> Float[Tensor, "batch posn d_model"]:
+        mu = t.mean(residual, dim=-1, keepdim=True)
+        var = t.var(residual, dim=-1, correction=0, keepdim=True)
+        residual = ((residual - mu) / t.sqrt(var + self.cfg.layer_norm_eps)) * self.w + self.b
+        return residual
+
+
+rand_float_test(LayerNorm, [2, 4, 768])
+load_gpt2_test(LayerNorm, reference_gpt2.ln_final, cache["resid_post", 11])
+tests.test_layer_norm_epsilon(LayerNorm, cache["resid_post", 11].cpu())
+
+# %%
+class Embed(nn.Module):
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.W_E = nn.Parameter(t.empty((cfg.d_vocab, cfg.d_model)))
+        nn.init.normal_(self.W_E, std=self.cfg.init_range)
+
+    def forward(self, tokens: Int[Tensor, "batch position"]) -> Float[Tensor, "batch position d_model"]:
+        return self.W_E[tokens]
+
+
+rand_int_test(Embed, [2, 4])
+load_gpt2_test(Embed, reference_gpt2.embed, tokens)
+
+
+
+# %%
+class PosEmbed(nn.Module):
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.W_pos = nn.Parameter(t.empty((cfg.n_ctx, cfg.d_model)))
+        nn.init.normal_(self.W_pos, std=self.cfg.init_range)
+
+    def forward(self, tokens: Int[Tensor, "batch position"]) -> Float[Tensor, "batch position d_model"]:
+        b, seq_len = tokens.shape
+        return einops.repeat(self.W_pos[:seq_len], "position d_model -> batch position d_model", batch=b)
+
+
+rand_int_test(PosEmbed, [2, 4])
+load_gpt2_test(PosEmbed, reference_gpt2.pos_embed, tokens)
+# %%
+
+# class Attention(nn.Module):
+#     IGNORE: Float[Tensor, ""]
+
+#     def __init__(self, cfg: Config):
+#         super().__init__()
+#         self.cfg = cfg
+#         self.register_buffer("IGNORE", t.tensor(float("-inf"), dtype=t.float32, device=device))
+
+#     def apply_causal_mask(
+#         self,
+#         attn_scores: Float[Tensor, "batch n_heads query_pos key_pos"],
+#     ) -> Float[Tensor, "batch n_heads query_pos key_pos"]:
+#         """
+#         Applies a causal mask to attention scores, and returns masked scores.
+#         """
+#         dq, dk = attn_scores.shape[-2:]
+#         mask = t.triu(t.ones(attn_scores.shape[-2:], device=attn_scores.device), diagonal=1).bool()
+#         attn_scores = t.where(mask, self.IGNORE, attn_scores)
+#         return attn_scores
+
+
+# tests.test_causal_mask(Attention.apply_causal_mask)
+
+
+
+# %%
+class Attention(nn.Module):
+    IGNORE: Float[Tensor, ""]
+
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.W_Q = nn.Parameter(t.empty((cfg.n_heads, cfg.d_model, cfg.d_head)))
+        self.W_K = nn.Parameter(t.empty((cfg.n_heads, cfg.d_model, cfg.d_head)))
+        self.W_V = nn.Parameter(t.empty((cfg.n_heads, cfg.d_model, cfg.d_head)))
+        self.W_O = nn.Parameter(t.empty((cfg.n_heads, cfg.d_head, cfg.d_model)))
+        self.b_Q = nn.Parameter(t.zeros((cfg.n_heads, cfg.d_head)))
+        self.b_K = nn.Parameter(t.zeros((cfg.n_heads, cfg.d_head)))
+        self.b_V = nn.Parameter(t.zeros((cfg.n_heads, cfg.d_head)))
+        self.b_O = nn.Parameter(t.zeros((cfg.d_model)))
+        nn.init.normal_(self.W_Q, std=self.cfg.init_range)
+        nn.init.normal_(self.W_K, std=self.cfg.init_range)
+        nn.init.normal_(self.W_V, std=self.cfg.init_range)
+        nn.init.normal_(self.W_O, std=self.cfg.init_range)
+        self.register_buffer("IGNORE", t.tensor(float("-inf"), dtype=t.float32, device=device))
+
+    def forward(self, normalized_resid_pre: Float[Tensor, "batch posn d_model"]) -> Float[Tensor, "batch posn d_model"]:
+
+        keys = einops.einsum(normalized_resid_pre, self.W_K, "b p d_model, h d_model d_head -> b p h d_head") + self.b_K
+        queries = einops.einsum(normalized_resid_pre, self.W_Q, "b p d_model, h d_model d_head -> b p h d_head") + self.b_Q
+        attn_scores = einops.einsum(queries, keys, "b q h d_head, b k h d_head -> b h q k") / (self.cfg.d_head ** 0.5)
+        attn_scores = self.apply_causal_mask(attn_scores) # (b, h, q, k)
+        attn_weights = t.softmax(attn_scores, dim=-1)
+
+
+        values = einops.einsum(normalized_resid_pre, self.W_V, "b p d_model, h d_model d_head -> b p h d_head") + self.b_V
+        z = einops.einsum(attn_weights, values, "b h q k, b k h d_head -> b h q d_head")
+        output = einops.einsum(z, self.W_O, "b h q d_head, h d_head d_model -> b q h d_model")
+        output = einops.reduce(output, "b q h d_model -> b q d_model", "sum") + self.b_O
+        return output
+
+    def apply_causal_mask(
+        self,
+        attn_scores: Float[Tensor, "batch n_heads query_pos key_pos"],
+    ) -> Float[Tensor, "batch n_heads query_pos key_pos"]:
+        """
+        Applies a causal mask to attention scores, and returns masked scores.
+        """
+        dq, dk = attn_scores.shape[-2:]
+        mask = t.triu(t.ones(attn_scores.shape[-2:], device=attn_scores.device), diagonal=1).bool()
+        attn_scores = t.where(mask, self.IGNORE, attn_scores)
+        return attn_scores
+
+
+
+tests.test_causal_mask(Attention.apply_causal_mask)
+rand_float_test(Attention, [2, 4, 768])
+load_gpt2_test(Attention, reference_gpt2.blocks[0].attn, cache["normalized", 0, "ln1"])
+# %%
+
+class MLP(nn.Module):
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.W_in = nn.Parameter(t.empty((cfg.d_model, cfg.d_mlp)))
+        self.W_out = nn.Parameter(t.empty((cfg.d_mlp, cfg.d_model)))
+        self.b_in = nn.Parameter(t.zeros((cfg.d_mlp)))
+        self.b_out = nn.Parameter(t.zeros((cfg.d_model)))
+        nn.init.normal_(self.W_in, std=self.cfg.init_range)
+        nn.init.normal_(self.W_out, std=self.cfg.init_range)
+
+    def forward(self, normalized_resid_mid: Float[Tensor, "batch posn d_model"]) -> Float[Tensor, "batch posn d_model"]:
+        res = einops.einsum(normalized_resid_mid, self.W_in, "b p d_model, d_model d_mlp -> b p d_mlp") + self.b_in
+        res = gelu_new(res)
+        res = einops.einsum(res, self.W_out, "b p d_mlp, d_mlp d_model -> b p d_model") + self.b_out
+        return res
+
+
+rand_float_test(MLP, [2, 4, 768])
+load_gpt2_test(MLP, reference_gpt2.blocks[0].mlp, cache["normalized", 0, "ln2"])
+# %%
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.ln1 = LayerNorm(cfg)
+        self.attn = Attention(cfg)
+        self.ln2 = LayerNorm(cfg)
+        self.mlp = MLP(cfg)
+
+    def forward(self, resid_pre: Float[Tensor, "batch position d_model"]) -> Float[Tensor, "batch position d_model"]:
+        x = self.ln1(resid_pre)
+        x = self.attn(x) + resid_pre
+        x2 = self.ln2(x)
+        x2 = self.mlp(x2) + x
+        return x2
+
+
+rand_float_test(TransformerBlock, [2, 4, 768])
+load_gpt2_test(TransformerBlock, reference_gpt2.blocks[0], cache["resid_pre", 0])
+# %%
+
+class Unembed(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.W_U = nn.Parameter(t.empty((cfg.d_model, cfg.d_vocab)))
+        nn.init.normal_(self.W_U, std=self.cfg.init_range)
+        self.b_U = nn.Parameter(t.zeros((cfg.d_vocab), requires_grad=False))
+
+    def forward(
+        self, normalized_resid_final: Float[Tensor, "batch position d_model"]
+    ) -> Float[Tensor, "batch position d_vocab"]:
+        return einops.einsum(normalized_resid_final, self.W_U, "b p d_model, d_model d_vocab -> b p d_vocab") + self.b_U
+
+
+rand_float_test(Unembed, [2, 4, 768])
+load_gpt2_test(Unembed, reference_gpt2.unembed, cache["ln_final.hook_normalized"])
+# %%
+
+class DemoTransformer(nn.Module):
+    def __init__(self, cfg: Config):
+        super().__init__()
+        self.cfg = cfg
+        self.embed = Embed(cfg)
+        self.pos_embed = PosEmbed(cfg)
+        self.blocks = nn.ModuleList([TransformerBlock(cfg) for _ in range(cfg.n_layers)])
+        self.ln_final = LayerNorm(cfg)
+        self.unembed = Unembed(cfg)
+
+    def forward(self, tokens: Int[Tensor, "batch position"]) -> Float[Tensor, "batch position d_vocab"]:
+        x = self.embed(tokens) + self.pos_embed(tokens) # (b, p, d_model)
+        for block in self.blocks:
+            x = block(x)
+        x = self.ln_final(x)
+        x = self.unembed(x)
+        return x
+
+
+rand_int_test(DemoTransformer, [2, 4])
+load_gpt2_test(DemoTransformer, reference_gpt2, tokens)
+# %%
+
+DemoTransformer(cfg).to(device)(tokens).shape
+# %%
+demo_gpt2 = DemoTransformer(Config(debug=False)).to(device)
+demo_gpt2.load_state_dict(reference_gpt2.state_dict(), strict=False)
+
+demo_logits = demo_gpt2(tokens)
+
+def get_log_probs(
+    logits: Float[Tensor, "batch posn d_vocab"], tokens: Int[Tensor, "batch posn"]
+) -> Float[Tensor, "batch posn-1"]:
+    log_probs = logits.log_softmax(dim=-1)
+    # Get logprobs the first seq_len-1 predictions (so we can compare them with the actual next tokens)
+    log_probs_for_tokens = log_probs[:, :-1].gather(dim=-1, index=tokens[:, 1:].unsqueeze(-1)).squeeze(-1)
+
+    return log_probs_for_tokens
+
+
+pred_log_probs = get_log_probs(demo_logits, tokens)
+print(f"Avg cross entropy loss: {-pred_log_probs.mean():.4f}")
+print(f"Avg cross entropy loss for uniform distribution: {math.log(demo_gpt2.cfg.d_vocab):4f}")
+print(f"Avg probability assigned to correct token: {pred_log_probs.exp().mean():4f}")
+# %%
+
+test_string = """Mitigating the risk of extinction from AI should be a global priority alongside other societal-scale risks such as"""
+for i in range(10):
+    test_tokens = reference_gpt2.to_tokens(test_string).to(device)
+    print(test_tokens)
+    demo_logits = demo_gpt2(test_tokens)
+    print(demo_logits.shape)
+    test_string += reference_gpt2.tokenizer.decode(demo_logits[-1, -1].argmax())
+    # for j in demo_logits[-1]:
+    #     print(reference_gpt2.tokenizer.decode(j.argmax()))
+
+# print(test_string)
+# %%
+model_cfg = Config(
+    debug=False,
+    d_model=32,
+    n_heads=16,
+    d_head=2,
+    d_mlp=32 * 4,
+    n_layers=4,
+    n_ctx=128,
+    d_vocab=reference_gpt2.cfg.d_vocab,
+)
+model = DemoTransformer(model_cfg)
+
+@dataclass
+class TransformerTrainingArgs:
+    batch_size: int = 32
+    epochs: int = 10
+    max_steps_per_epoch: int = 500
+    lr: float = 1e-3
+    weight_decay: float = 1e-2
+    wandb_project: str | None = "day1-demotransformer"
+    wandb_name: str | None = None
+
+
+args = TransformerTrainingArgs()
+
+dataset = datasets.load_dataset("roneneldan/TinyStories", split="train")
+print(dataset)
+print(dataset[0]["text"])
+
+# %%
+
+tokenized_dataset = tokenize_and_concatenate(
+    dataset,
+    reference_gpt2.tokenizer,
+    streaming=False,
+    max_length=model.cfg.n_ctx,
+    column_name="text",
+    add_bos_token=True,
+    num_proc=4,
+)
+
+dataset_dict = tokenized_dataset.train_test_split(test_size=1000)
+train_loader = DataLoader(
+    dataset_dict["train"], batch_size=args.batch_size, shuffle=True, num_workers=4, pin_memory=True
+)
+test_loader = DataLoader(
+    dataset_dict["test"], batch_size=args.batch_size, shuffle=False, num_workers=4, pin_memory=True
+)
+# %%
+first_batch = train_loader.dataset[: args.batch_size]
+
+print(first_batch.keys())
+print(first_batch["tokens"].shape)
+# %%
+
+class TransformerTrainer:
+    def __init__(self, args: TransformerTrainingArgs, model: DemoTransformer):
+        super().__init__()
+        self.model = model
+        self.args = args
+        self.sampler = solutions.TransformerSampler(self.model, reference_gpt2.tokenizer)
+        self.optimizer = t.optim.AdamW(self.model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+        self.step = 0
+
+        self.train_loader = DataLoader(
+            dataset_dict["train"],
+            batch_size=args.batch_size,
+            shuffle=True,
+            num_workers=4,
+            pin_memory=True,
+        )
+        self.test_loader = DataLoader(
+            dataset_dict["test"],
+            batch_size=args.batch_size,
+            shuffle=False,
+            num_workers=4,
+            pin_memory=True,
+        )
+
+    def training_step(self, batch: dict[str, Int[Tensor, "batch seq"]]) -> Float[Tensor, ""]:
+        """
+        Calculates the loss on the tokens in the batch, performs a gradient update step, and logs the loss.
+
+        Remember that `batch` is a dictionary with the single key 'tokens'.
+        """
+        raise NotImplementedError()
+        return loss
+
+    @t.inference_mode()
+    def evaluate(self) -> float:
+        """
+        Evaluate the model on the test set and return the accuracy.
+        """
+        self.model.eval()
+        #
+        # YOUR CODE HERE - fill in the `evaluate` method
+        #
+        self.model.train()
+        return accuracy
+
+    def train(self):
+        """
+        Trains the model, for `self.args.epochs` epochs. Also handles wandb initialisation, and early stopping
+        for each epoch at `self.args.max_steps_per_epoch` steps.
+        """
+        wandb.init(project=self.args.wandb_project, name=self.args.wandb_name, config=self.args)
+        accuracy = np.nan
+
+        progress_bar = tqdm(total=self.args.max_steps_per_epoch * self.args.epochs)
+
+        for epoch in range(self.args.epochs):
+            for i, batch in enumerate(self.train_loader):
+                loss = self.training_step(batch)
+                progress_bar.update()
+                progress_bar.set_description(f"Epoch {epoch + 1}, loss: {loss:.3f}, accuracy: {accuracy:.3f}")
+                if i >= self.args.max_steps_per_epoch:
+                    break
+
+            accuracy = self.evaluate()
+            sample_text = self.sampler.sample("Once upon a time", max_tokens_generated=50)
+            print(sample_text)
+
+        wandb.finish()
+
+
+# See the full run here: https://api.wandb.ai/links/dquarel/nrxuwnv7
+model = DemoTransformer(model_cfg).to(device)
+args = TransformerTrainingArgs()
+trainer = TransformerTrainer(args, model)
+trainer.train()
